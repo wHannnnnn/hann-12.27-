f9742724aacf2016ede5d8c96e114f1bc51c264d
wHannnnnn <2865430783@qq.com>
2022-01-17 10:39:47 +0800

    112

--- src/components/home/banner.vue
+++ src/components/home/banner.vue
@@ -1,6 +1,6 @@
 <template>
    <van-swipe :autoplay="3000" indicator-color="white" :stop-propagation="false">
-        <van-swipe-item v-for="item in bannerList" :key="item.id">
+        <van-swipe-item v-for="item in bannerList">
             <img v-lazy="item.picUrl" alt="">
         </van-swipe-item>
    </van-swipe>
